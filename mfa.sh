81bfab4aaadcb4f3379f72427ded5a90fa572095
elise-lin <32940564+elise-lin@users.noreply.github.com>
2020-03-01 09:47:10 -0800

    Final

--- mfa.sh
+++ mfa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Pretty shitty script that prints out curr line then deletes it just so I don't have to vim all da time
+# Returns passcode, then deletes it. Passcodes must be at top.
 
 FILE="ucr_mfa_passcodes.txt"
 
 # print out first line
 head -n 1 $FILE
-#delete first line, -i = modify file
-sed -i '1d' $FILE
+#delete first line + newline, -i = modify file
+sed -i 1,2d $FILE
